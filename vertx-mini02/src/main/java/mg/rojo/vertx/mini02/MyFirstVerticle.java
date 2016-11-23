6c35af47dc54a0b655a5ca22139e16949365e97f
dida <dida.ratsimba@gmail.com>
2016-11-24 01:26:56 +0300

    you don't need main class anymore name refactoring formatting code clean code add better class name and parameters name

--- vertx-mini02/src/main/java/mg/rojo/vertx/mini02/MyFirstVerticle.java
+++ vertx-mini02/src/main/java/mg/rojo/vertx/mini02/MyFirstVerticle.java
@@ -2,21 +2,22 @@ package mg.rojo.vertx.mini02;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
-public class MyFirstVerticleTest extends AbstractVerticle {
+public class MyFirstVerticle extends AbstractVerticle {
 
   @Override
-  public void start(Future<Void> fut) {
+  public void start(Future<Void> startFuture) {
     vertx
         .createHttpServer()
         .requestHandler(r -> {
           r.response().end("<h1>Hello from my first " +
               "Vert.x 3 application</h1>");
         })
+            //FIXME : run on port 8668 !!!!
         .listen(8080, result -> {
           if (result.succeeded()) {
-            fut.complete();
+              startFuture.complete();
           } else {
-            fut.fail(result.cause());
+              startFuture.fail(result.cause());
           }
         });
   }
